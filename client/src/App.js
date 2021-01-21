c14d9c4e77a47897c6f4d969b81636574762f556
Jose Jimenez <jrjimenez221@gmail.com>
2021-01-20 18:51:19 -0600

    preparing for merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-export default App;
-
-
-
 // import React from "react";
-// import Wrapper from "./components/Wrap";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
 
-// export default function App() {
+// function App() {
 //   return (
-//     <div styleName="margin:0px; padding:0px;">
-//       <Wrapper/>
-//     </div>
-//   )
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path={["/", "/books"]}>
+//             <Books />
+//           </Route>
+//           <Route exact path="/books/:id">
+//             <Detail />
+//           </Route>
+//           <Route>
+//             <NoMatch />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
 // }
+// export default App;
+
+import React from "react";
+import Wrapper from "./components/Wrap";
+
+export default function App() {
+  return (
+    <div styleName="margin:0px; padding:0px;">
+      <Wrapper/>
+    </div>
+  )
+};
