7fcfa06c082bf53c5271b9ac31008e62d3404925
stephdanid <stephaniedanidominguez@gmail.com>
2021-01-22 12:32:16 -0600

    added signup buton, added an api route for signIn freom the homepage

--- client/src/components/Layout/SignIn/index.js
+++ client/src/components/Layout/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
 import {
   Route,
@@ -13,14 +13,21 @@ import API from "../../../utils/API.js"
 
 import linkedinImg from "../../misc/images/linkedin.png";
 // const appLink = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile%20r_emailaddress&client_id=78o6w4y2569mqx&redirect_uri=https%3A%2F%2Felation-station.herokuapp.com%2F";
+
 function SignIn() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   function signIn(event) {
     event.preventDefault();
 console.log(event.target)
+console.log(emailRef.current.value, passwordRef.current.value)
+API.signIn({email:emailRef.current.value, password:passwordRef.current.value})
   }
   function signUp(event) {
     event.preventDefault();
 console.log(event.target)
+// console.log(email:emailRef.current.value, password:passwordRef.current.value)
 API.signUp()
   }
   return (
@@ -31,7 +38,7 @@ API.signUp()
           className="col-11 col-md-3 signInPrompt p-3 mt-3 "
           style={{ paddingBottom: "2vh", borderRadius: "10%" }}
         >
-          <form>
+          <form onSubmit={signIn}>
             <div className="form-group">
               <input
                 type="email"
@@ -39,6 +46,8 @@ API.signUp()
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
+               
+                ref={emailRef}
               />
             </div>
 
@@ -48,15 +57,19 @@ API.signUp()
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
+                ref = {passwordRef}
               />
             </div>
 
             <div className="row justify-content-between pl-4 pr-4">
-              <button type="submit" class="btn btn-primary col-5">
-                <Link to="/station">Sign In</Link>
+              <button type="submit"  class="btn btn-primary col-5">
+             Sign In
+              </button>
+              <button type="button" onClick={signUp} class="btn btn-primary col-5">
+             Sign Up
               </button>
               <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile%20r_emailaddress&client_id=78o6w4y2569mqx&redirect_uri=https%3A%2F%2Felation-station.herokuapp.com%2F"type="submit" class="btn btn-primary col-5">
-              
+              LinkedIn
               <img id="linkinImg" style={{width:"100%"}}src={linkedinImg}></img>
               </a>
               
