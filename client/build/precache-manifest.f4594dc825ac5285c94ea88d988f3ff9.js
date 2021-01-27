d1345ed64bbb30e1c15da62b331f02083fa3b48f
Jose Jimenez <jrjimenez221@gmail.com>
2021-01-27 00:05:30 -0600

    resolved conflicts

---
+++ client/build/precache-manifest.f4594dc825ac5285c94ea88d988f3ff9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016ffa9bbd79ffdb905acde0ac57e899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c08a72701ebaa74f895",
+    "url": "/static/css/2.4210231d.chunk.css"
+  },
+  {
+    "revision": "0fa064361c1ba5637a8f",
+    "url": "/static/css/main.fdbe5721.chunk.css"
+  },
+  {
+    "revision": "3c08a72701ebaa74f895",
+    "url": "/static/js/2.de90568f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.de90568f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa064361c1ba5637a8f",
+    "url": "/static/js/main.db7f8928.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "0a0c08cdb430cabdaca4b8c7dac22648",
+    "url": "/static/media/backgroundSample.0a0c08cd.png"
+  },
+  {
+    "revision": "b2d5212dec30892cfefdf73f8d3bec59",
+    "url": "/static/media/bg2.b2d5212d.jpg"
+  }
+]);
